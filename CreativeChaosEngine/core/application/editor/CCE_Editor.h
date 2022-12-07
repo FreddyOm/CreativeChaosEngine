dde43c601ad3f73db48ebd1a190bf08d86d02fb6
Frederik Omlor <freddy-omlor@web.de>
2022-12-07 23:35:53 +0100

    no message

--- CreativeChaosEngine/core/application/editor/CCE_Editor.h
+++ CreativeChaosEngine/core/application/editor/CCE_Editor.h
@@ -15,6 +15,7 @@ class CCE_Editor
 #if _DEBUG
 	friend struct EditorTest;
 #endif // unit testing
+
 private:
 	bool editorRunning = false;
 	HINSTANCE hInstance = {};
@@ -24,6 +25,7 @@ public:
 #if _DEBUG
 	CCE_Editor();
 #endif // unit testing
+
 	CCE_Editor(HINSTANCE _hInstance);
 	~CCE_Editor();
 	int OpenEditor() noexcept;
