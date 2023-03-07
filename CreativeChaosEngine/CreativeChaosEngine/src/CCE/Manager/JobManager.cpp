d41f685b26c22aefeb68410bbd2036ad21c247f9
Frederik Omlor <freddy-omlor@web.de>
2023-03-07 20:14:26 +0100

    NOT FINISHED! Added job structures to Job Manager

--- CreativeChaosEngine/CreativeChaosEngine/src/CCE/Manager/JobManager.cpp
+++ CreativeChaosEngine/CreativeChaosEngine/src/CCE/Manager/JobManager.cpp
@@ -5,6 +5,9 @@
 
 namespace CCE
 {
+	/// <summary>
+	/// Starts up the job manager.
+	/// </summary>
 	void JobManager::StartUp()
 	{
 		static JobManager* Instance;
@@ -12,12 +15,17 @@ namespace CCE
 		Instance = this;
 
 		auto startTime = Time::CurrentTick();
+		SpawnWorkerThreads();
 		initialized = true;
 
 		auto endTime = Time::CurrentTick();
 		double initDuration = Time::GetDurationInMicroSec(startTime, endTime);
 		LOGC("JobManager initialized!", COLOR_BLUE);
 	}
+
+	/// <summary>
+	/// Shuts down the job manager.
+	/// </summary>
 	void JobManager::ShutDown()
 	{
 		LOGC("Shutting down JobManager...", COLOR_BLUE);
@@ -25,5 +33,109 @@ namespace CCE
 		Instance = nullptr;
 	}
 
+	/// <summary>
+	/// Spawns the worker threads.
+	/// </summary>
+	/// <param name="numOfThreads"></param>
+	void JobManager::SpawnWorkerThreads(short numOfThreads)
+	{
+#ifdef CCE_PLATFORM_WINDOWS // PLATFORM WINDOWS
+
+		SpawnWorkerThreadsWin(numOfThreads);
+
+#else
+#error CCE is currently only supported for Windows
+#endif // CCE_PLATFORM_WINDOWS
+
+	}
+
+	/// <summary>
+	/// Kicks a job.
+	/// </summary>
+	/// <param name="decl">Declaration of the job.</param>
+	/// <returns>True if job was successfully kicked, false if an error occured.</returns>
+	bool JobManager::KickJob(const Job::Declaration& decl)
+	{
+		Job _job = Job(decl);
+		switch(decl.m_priority)
+		{
+		case Job::Priority::HIGH:
+		{
+			jobQueue_High.push(_job);
+			break;
+		}
+		case Job::Priority::NORMAL:
+		{
+			jobQueue_Normal.push(_job);
+			break;
+		}
+		default:
+		{
+			jobQueue_Low.push(_job);
+			break;
+		}
+		}
+
+		return true;
+	}
+
+	/// <summary>
+	/// Kicks multiple jobs in a row.
+	/// </summary>
+	/// <param name="count">The amount of jobs to kick.</param>
+	/// <param name="decls">The declarations of the jobs.</param>
+	/// <returns>True if jobs were successfully kicked, false if an error occured.</returns>
+	bool JobManager::KickJobs(int count, const JobManager::Job::Declaration decls[])
+	{
+		bool success = true;
+		for (unsigned short i = 0; i < count; i++)
+		{
+			if (!KickJob(decls[i]))
+			{
+				success = false;
+			}
+		}
+
+		return success;
+	}
+
+	/// <summary>
+	/// Spawns multiple worker threads. Windows only version.
+	/// </summary>
+	/// <param name="numOfThreads">The number of threads to spawn. 
+	/// If set to -1, the number will be set to the number of physical cores.</param>
+	void JobManager::SpawnWorkerThreadsWin(short numOfThreads)
+	{
+		if (numOfThreads == -1)
+		{
+			// handle default worker threads amount
+			numOfThreads = std::thread::hardware_concurrency();
+		}
+		
+		LOG_JOBS("Number of physical cpu cores: %i", numOfThreads);
+
+		mainFiber = ConvertThreadToFiber(NULL);
+		DASSERT(mainFiber != nullptr, "Conversion main thread -> fiber not succesful!");
+		
+		worker_threads = std::vector<std::thread>(numOfThreads);
+
+		for (unsigned short t_index = 0; t_index < numOfThreads; t_index++)
+		{
+			// spawn threads with cpu affinity
+
+		}
+
+		/*
+		cpu_set_t cpuset;
+		CPU_ZERO(&cpuset);
+		CPU_SET(i, &cpuset);
+		int rc = pthread_setaffinity_np(threads[i].native_handle(),
+			sizeof(cpu_set_t), &cpuset);
+			*/
+	}
+
+	/// <summary>
+	/// Singelton instance of the job manager.
+	/// </summary>
 	JobManager* JobManager::Instance = nullptr;
 }
--- CreativeChaosEngine/CreativeChaosEngine/src/CCE/Manager/JobManager.h
+++ CreativeChaosEngine/CreativeChaosEngine/src/CCE/Manager/JobManager.h
@@ -1,5 +1,8 @@
 #pragma once
 #include "BaseManager.h"
+#include <thread>
+#include <vector>
+#include <queue>
 
 namespace CCE
 {
@@ -13,5 +16,74 @@ namespace CCE
 		void ShutDown() override;
 
 		static JobManager* Instance;
+	
+	protected:
+		struct Job
+		{
+			typedef void EntryPoint(uintptr_t param);
+			enum class Priority
+			{
+				HIGH,
+				NORMAL,
+				LOW
+			};
+
+			struct Counter
+			{
+
+			};
+
+			struct Declaration
+			{
+				EntryPoint* m_pEntryPoint;
+				va_list m_param;
+				Priority m_priority;
+				Counter* m_pCounter;
+			};
+
+			Job(Declaration decl)
+			{
+				id = g_index++;
+				
+				m_pEntrancePoint = decl.m_pEntryPoint;
+				m_params = decl.m_param;
+				m_pCounter = decl.m_pCounter;
+				m_priority = decl.m_priority;
+			}
+			static unsigned int g_index;
+			unsigned int id = 0;
+
+			EntryPoint* m_pEntrancePoint = nullptr;
+			va_list m_params= nullptr;
+			Priority m_priority = Priority::LOW;
+			Counter* m_pCounter = nullptr;
+		};
+
+		struct Fiber
+		{
+			struct FiberContext
+			{
+				// Registers
+				// Stack space
+			};
+
+			unsigned int id = 0;
+			FiberContext cntxt;
+		};
+		void SpawnWorkerThreads(short numOfThreads = -1);
+		bool KickJob(const Job::Declaration& decl);
+		bool KickJobs(int count, const Job::Declaration decls[]);
+	private:
+		void SpawnWorkerThreadsWin(short numOfThreads = -1);
+		
+	private:
+		LPVOID mainFiber = nullptr;
+		std::vector<std::thread> worker_threads;
+		std::vector<Fiber> fiber_pool;
+		std::vector<Job> wait_list;
+		
+		std::queue<Job> jobQueue_High;
+		std::queue<Job> jobQueue_Normal;
+		std::queue<Job> jobQueue_Low;
 	};
 }
