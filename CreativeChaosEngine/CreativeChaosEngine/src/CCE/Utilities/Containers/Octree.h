510056191b591be3fbd02c50644fe14a4d15688c
Frederik Omlor <freddy.omlor@korion.de>
2024-01-20 22:29:46 +0100

    Fixed issue and restructured some code.

--- CreativeChaosEngine/CreativeChaosEngine/src/CCE/Utilities/Containers/Octree.h
+++ CreativeChaosEngine/CreativeChaosEngine/src/CCE/Utilities/Containers/Octree.h
@@ -7,6 +7,8 @@
 #include "../../Graphics/Rendering/D3D11.h"
 #include <functional>
 #include <vector>
+#include "../../ECS/Entity.h"
+#include "../../Physics/Physics.h"
 
 namespace CCE::Containers
 {
@@ -27,7 +29,7 @@ namespace CCE::Containers
 			DirectX::XMStoreFloat3(&size, _size);
 		}
 
-		T* pObjRef = nullptr;
+		const T* pObjRef = nullptr;
 		DirectX::XMFLOAT3 pos{};
 		DirectX::XMFLOAT3 size{};
 	};
@@ -38,15 +40,98 @@ namespace CCE::Containers
 	public:
 		typedef std::function<void(std::vector<OctreeEntry<T>>&)> OctreeCallback;
 
-		void OperateOnContents(OctreeCallback& callback);
-		void Split();
+		/// <summary>
+		/// Calls the callback function on the children of the tree or, if none
+		/// exist, in its content.
+		/// </summary>
+		/// <typeparam name="T">The data type of the stored data.</typeparam>
+		/// <param name="callback">A callback to the </param>
+		void OperateOnContents(OctreeCallback& callback)
+		{
+			if (nullptr != children) {
+				for (int i = 0; i < 8; ++i)			// Octree can have up to 8 children
+				{
+					children[i].OperateOnContents(callback);
+				}
+			}
+			else
+			{
+				if (!contents.empty())
+				{
+					callback(contents);
+				}
+			}
+		}
+
+		void Split()
+		{
+			using namespace DirectX;
+
+			XMFLOAT3 halfSize;
+			XMStoreFloat3(&halfSize, XMLoadFloat3(&size) / 2.0f);
+
+			XMFLOAT3 position0; XMStoreFloat3(&position0, XMVectorAdd(XMLoadFloat3(&position), { -halfSize.x, -halfSize.y, -halfSize.z }));
+			XMFLOAT3 position1; XMStoreFloat3(&position1, XMVectorAdd(XMLoadFloat3(&position), { halfSize.x, -halfSize.y, -halfSize.z }));
+			XMFLOAT3 position2; XMStoreFloat3(&position2, XMVectorAdd(XMLoadFloat3(&position), { halfSize.x, -halfSize.y, halfSize.z }));
+			XMFLOAT3 position3; XMStoreFloat3(&position3, XMVectorAdd(XMLoadFloat3(&position), { -halfSize.x, -halfSize.y, halfSize.z }));
+			XMFLOAT3 position4; XMStoreFloat3(&position4, XMVectorAdd(XMLoadFloat3(&position), { -halfSize.x, halfSize.y, -halfSize.z }));
+			XMFLOAT3 position5; XMStoreFloat3(&position5, XMVectorAdd(XMLoadFloat3(&position), { halfSize.x, halfSize.y, -halfSize.z }));
+			XMFLOAT3 position6; XMStoreFloat3(&position6, XMVectorAdd(XMLoadFloat3(&position), { halfSize.x, halfSize.y, halfSize.z }));
+			XMFLOAT3 position7; XMStoreFloat3(&position7, XMVectorAdd(XMLoadFloat3(&position), { -halfSize.x, halfSize.y, halfSize.z }));
+
+			children = new OctreeNode<T>[8];
+
+			children[0] = OctreeNode<T>(position0, halfSize);
+			children[1] = OctreeNode<T>(position1, halfSize);
+			children[2] = OctreeNode<T>(position2, halfSize);
+			children[3] = OctreeNode<T>(position3, halfSize);
+			children[4] = OctreeNode<T>(position4, halfSize);
+			children[5] = OctreeNode<T>(position5, halfSize);
+			children[6] = OctreeNode<T>(position6, halfSize);
+			children[7] = OctreeNode<T>(position7, halfSize);
+		}
+
 		void Insert(const T& objectRef, const DirectX::XMFLOAT3& objectPos,
-			const DirectX::XMFLOAT3& objectSize, int depthThreshold, const int maxSize);
+			const DirectX::XMFLOAT3& objectSize, int depthThreshold, const int maxSize)
+		{
+			// Check AABB. Does the other object belong in here?
+			if (!CCE::Physics::CollideAABB(objectPos, objectSize, position, size))
+			{
+				return;
+			}
+
+			if (children)
+			{
+				// Doesn't belong here! Put in children
+				for (int i = 0; i < 8; ++i) {
+					children[i].Insert(objectRef, objectPos, objectSize, depthThreshold - 1, maxSize);
+				}
+			}
+			else
+			{
+				contents.push_back(OctreeEntry<T>(&objectRef, objectPos, objectSize));
+
+				// put contents in sub branches
+				if (contents.size() > maxSize && depthThreshold > 0)
+				{
+					Split();
+
+					for (const auto& content : contents)
+					{
+						for (int i = 0; i < 8; ++i)
+						{
+							children[i].Insert(*content.pObjRef, content.pos, content.size, depthThreshold - 1, maxSize);
+						}
+					}
+					contents.clear(); // clear content!
+				}
+			}
+		}
 
 	protected:
 		friend class Octree<T>;
 		OctreeNode() { contents.reserve(5); }
-		OctreeNode(const DirectX::XMFLOAT3& _position, const DirectX::XMFLOAT3& _size)
+		OctreeNode(const DirectX::XMFLOAT3 _position, const DirectX::XMFLOAT3& _size)
 			: position(_position), size(_size), children(nullptr)
 		{ contents.reserve(5); }
 		~OctreeNode() { delete[] children; }
